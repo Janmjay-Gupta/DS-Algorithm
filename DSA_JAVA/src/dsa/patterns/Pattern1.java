5a60f2da3c78ea1eb8cb19c4a7e05d947d53a9ea
Janmjay <janmjaygupta111@gmail.com>
2022-11-25 23:52:16 +0530

    first pattern question solved

--- DSA_JAVA/src/dsa/patterns/Pattern1.java
+++ DSA_JAVA/src/dsa/patterns/Pattern1.java
@@ -1,9 +1,24 @@
 package dsa.patterns;
 
+import java.util.Scanner;
+
 public class Pattern1 {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("Please enter a number to form the pattern : ");
+		int n = sc.nextInt();
+		
+		for(int i = 0; i < n; i++) {
+			
+			for(int j = 0; j <= i; j++) {
+				
+				System.out.print("*\t");
+			}
+			
+			System.out.println();
+		}
 	}
 
 }
