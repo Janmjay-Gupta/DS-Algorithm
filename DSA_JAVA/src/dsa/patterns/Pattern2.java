0392d533b6fa7ea77d94ac06722d74a7095300b9
Janmjay <janmjaygupta111@gmail.com>
2022-11-26 00:10:09 +0530

    second pattern question solved

--- DSA_JAVA/src/dsa/patterns/Pattern2.java
+++ DSA_JAVA/src/dsa/patterns/Pattern2.java
@@ -2,7 +2,7 @@ package dsa.patterns;
 
 import java.util.Scanner;
 
-public class Pattern1 {
+public class Pattern2 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -12,13 +12,12 @@ public class Pattern1 {
 		
 		for(int i = 0; i < n; i++) {
 			
-			for(int j = 0; j <= i; j++) {
+			for(int j = n; j > i; j--) {
 				
 				System.out.print("*\t");
 			}
-			System.out.println(); 
+			System.out.println();
 		}
 	}
 
 }
- 
