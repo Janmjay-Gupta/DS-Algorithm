c5d17cd3356bd7e335f565613cde1e90dbcfa817
Janmjay <janmjaygupta111@gmail.com>
2022-11-26 00:44:50 +0530

    third pattern question solved

--- DSA_JAVA/src/dsa/patterns/Pattern3.java
+++ DSA_JAVA/src/dsa/patterns/Pattern3.java
@@ -2,33 +2,35 @@ package dsa.patterns;
 
 import java.util.Scanner;
 
-public class Pattern1 {
+public class Pattern3 {
+	
 	
 	/*
 	
-	*
-	*	*
-	*	* 	*
-	*	*	*	*
+					*
+				*	*
+			*	* 	*
+		*	*	*	*
 	*	*	*	*	*
 	
 	**/
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
 		System.out.println("Please enter a number to form the pattern : ");
 		int n = sc.nextInt();
 		
 		for(int i = 0; i < n; i++) {
 			
+			for(int j = n-1; j > i; j--) {
+				System.out.print("\t");
+			}
 			for(int j = 0; j <= i; j++) {
-				
 				System.out.print("*\t");
 			}
-			System.out.println(); 
+			
+			System.out.println();
 		}
 	}
 
 }
- 
