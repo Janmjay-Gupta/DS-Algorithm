fc664407011719add68607a76e26729363cec0fa
Janmjay <janmjaygupta111@gmail.com>
2022-11-26 17:08:11 +0530

    fourth pattern question solved

---
+++ DSA_JAVA/src/dsa/patterns/Pattern4.java
@@ -0,0 +1,35 @@
+package dsa.patterns;
+
+import java.util.Scanner;
+
+public class Pattern4 {
+
+	/*
+	
+	
+	*	*	*	*	*	
+		*	*	*	*	
+			*	*	*	
+				*	*	
+					*	
+	
+	*/
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please enter a number to form the pattern : ");
+		int n = sc.nextInt();
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < i; j++) {
+				System.out.print("\t");
+			}
+			for(int j = n; j > i; j--) {
+				System.out.print("*\t");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
